c9fbe0776ddecea09e118afadeaf03df85e09fad
Gustavo <gusstavocta@gmail.com>
2025-01-06 22:13:54 -0500

    implementando migracion y creando tabla medicos

--- src/main/java/med/voll/api/direccion/Direccion.java
+++ src/main/java/med/voll/api/direccion/Direccion.java
@@ -12,9 +12,16 @@ import lombok.NoArgsConstructor;
 public class Direccion {
 
     private String calle;
-    private Integer numero;
+    private String numero;
     private String complemento;
     private String distrito;
     private String ciudad;
 
+    public Direccion(DatosDireccion direccion) {
+        this.calle = direccion.calle();
+        this.numero = direccion.numero();
+        this.complemento = direccion.complemento();
+        this.distrito = direccion.distrito();
+        this.ciudad = direccion.ciudad();
+    }
 }
