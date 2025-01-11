e5c93e3aa54279d13cd5975a59063e362b5d85ac
Gustavo <gusstavocta@gmail.com>
2025-01-10 23:40:52 -0500

    paginacion

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -8,6 +8,7 @@ import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -30,7 +31,7 @@ public class MedicoController {
 //    }
 
     @GetMapping
-    public Page<DatosListadoMedico> listadoMedicos(Pageable paginacion) {
+    public Page<DatosListadoMedico> listadoMedicos(@PageableDefault( size=2) Pageable paginacion) {
         return medicoRepository.findAll(paginacion).map(DatosListadoMedico::new);
     }
 
